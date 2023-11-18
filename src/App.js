bf0f851580e64cf450d9af499844a01ffba45d33
ATTAPOL KEDRAMJIAK <6302739@rsu.ac.th>
2023-11-18 23:13:15 +0700

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Main from './components/Main';
-
 function App() {
   return (
     <div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import videoBg from '../assets/VideoFootage.mp4';
+import Modal from './Modal';
 
 
 const Main = () => {
@@ -10,10 +11,12 @@ const Main = () => {
         <div className="content">
             <h1>ยินดีต้อนรับ</h1>
             <p>โปรเจค Midterm วิชา CPE499 (React.JS)</p> 
+            <span>Souce Video มาจาก www.videvo.net/video/wind-blowing-in-the-desert</span> 
            <div className="textName">
             <h3 >อรรถพล เกิดลำเจียก</h3>
             <h3 >รหัสนักศึกษา 6302739</h3>
             </div>  
+            <Modal/>
             </div>  
       </div>
   );
